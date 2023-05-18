77ed67180a2695c5f5249d43ac41877e0179486f
Yusuf <yusuf.borucu@videoon.ly>
2023-05-18 11:25:57 +0300

    sw updated

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
 self.addEventListener('push', (e) => {
   const config = {
-    body: "Mesaj içeriği",
+    body: e.data.text() || "Mesaj içeriği",
     data: {
       dateOfArrival: Date.now(),
       primaryKey: "3"
-    }
+    },
+    icon: "images/logo.png",
+    vibrate: [100, 50, 100]
   }
 
   e.waitUntil(self.registration.showNotification("Yeni makale eklendi", config))
